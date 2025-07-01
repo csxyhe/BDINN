680a29dd9f55229ca597c32c56318369ffbaf30b
EllenHo <66475403+csxyhe@users.noreply.github.com>
2025-07-01 18:26:31 +0800

    Update BDeformConv_layer.py

--- BDeformConv_layer.py
+++ BDeformConv_layer.py
@@ -6,7 +6,7 @@ import thop
 
 
 class BDeformConv(nn.Module):
-    def __init__(self, inc, outc, kernel_size=3, padding=1, stride=1, bias=None, max_sscale=3, min_sscale=0.5, isReScale=True):
+    def __init__(self, inc, outc, kernel_size=3, stride=1, bias=None, max_sscale=3, min_sscale=0.5, isReScale=True):
         """
         args:
             max_sscale: (optional) default set is 3, can be set to `None`. Max threshold for the stretch scalar.
@@ -28,16 +28,20 @@ class BDeformConv(nn.Module):
         assert (min_sscale is None and max_sscale is None) or (min_sscale is not None and max_sscale is not None), \
             "Both max_sscale and min_sscale must be set simultaneously or both must be None"
         assert min_sscale < max_sscale, "max_sscale must be larger than min_sscale"
-        self.conv = nn.Conv2d(inc, outc, kernel_size=kernel_size, padding=padding, stride=stride, bias=bias)
-        if isinstance(padding, tuple):
-            self.padding = padding
-        else:
-            self.padding = (padding, padding)
+
         if isinstance(stride, tuple):
             self.stride = stride
         else:
             self.stride = (stride, stride)
-        base_points = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 0], [0, 1], [1, -1], [1, 0], [1, 1]]
+        if isinstance(kernel_size, int):
+            kernel_size = (kernel_size, kernel_size)
+        assert isinstance(kernel_size, tuple), "kernel_size must be tuple or int type"
+        padding = ((kernel_size[0] - 1) // 2, (kernel_size[1] - 1) // 2)
+        self.padding = padding
+
+        self.conv = nn.Conv2d(inc, outc, kernel_size=kernel_size, padding=padding, stride=stride, bias=bias)
+
+        base_points = self.generate_base_points(kernel_size)
         # (2, k)
         base_points = torch.tensor(base_points).permute(1, 0)
         # (1, 1, 1, 2, k)
@@ -83,6 +87,14 @@ class BDeformConv(nn.Module):
         nn.init.constant_(self.conv_rotation.bias[0], 0)
         nn.init.constant_(self.conv_rotation.bias[1], 1)
 
+    def generate_base_points(self, kernel_size):
+        kh, kw = kernel_size
+        base_points = []
+        for i in range(kh):
+            for j in range(kw):
+                base_points.append([i - kh // 2, j - kw // 2])
+        return base_points
+
     def forward(self, x):
         b, c, h, w = x.shape
         rotation = self.conv_rotation(x)
@@ -111,7 +123,7 @@ class BDeformConv(nn.Module):
         banded_offset[..., 0, :] *= r
         # apply the re-scale scalar for all points
         if self.isReScale:
-            wr = 1 + F.softplus(self.conv_whole(x))
+            wr = 1 + F.relu(self.conv_whole(x))
             # (b, h, w, 2)
             wr = wr.permute(0, 2, 3, 1).unsqueeze(-1)
             banded_offset *= wr
@@ -127,7 +139,7 @@ if __name__ == "__main__":
     channel = 16
     out_channel = 32
     height = width = 256
-    model = BDeformConv(channel, out_channel)
+    model = BDeformConv(channel, out_channel, 3)
     flops, params = thop.profile(model, inputs=(torch.randn(1, channel, height, width),), verbose=False)
     print(f"model FLOPs: {flops / (10 ** 9)}G")
     print(f"model Params: {params / (10 ** 6)}M")
